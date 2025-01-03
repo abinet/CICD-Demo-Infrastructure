d8123d96cf83ca4c0e85f828bf1702df4c40cbe1
Andriy Binetsky <abinet@gmail.com>
2025-01-03 13:43:20 +0100

    change to appset

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "argocd" {
-  repository = "oci://registry-1.docker.io/bitnamicharts"
-  version = "4.7.21"
+  repository = "https://argoproj.github.io/argo-helm"
+  version = "7.7.12"
   name = "argo-cd"
   chart = "argo-cd"
   namespace = "argocd"
@@ -29,35 +29,50 @@ resource "helm_release" "argocd" {
   }
 }
 
-resource "kubectl_manifest" "example_app_ns" {
-  yaml_body = <<EOT
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: example-app
-EOT
-}
-
-resource "kubectl_manifest" "argo_cd_config" {
+resource "kubectl_manifest" "argocd_appset" {
   yaml_body = <<EOT
 apiVersion: argoproj.io/v1alpha1
-kind: Application
+kind: ApplicationSet
 metadata:
-  name: example-app
+  name: example-appset
   namespace: argocd
 spec:
-  project: default
-  source:
-    repoURL: "https://github.com/DarthBlair/CICD-Demo-Deployment.git"
-    targetRevision: HEAD
-    path: k8s
-  destination:
-    server: "https://kubernetes.default.svc"
-    namespace: example-app
-EOT
+  goTemplate: true
+  #goTemplateOptions: ["missingkey=error"]
+  generators:
+  - git:
+      repoURL: https://github.com/abinet/CICD-Demo-Deployment.git
+      revision: add-config
+      directories:
+      - path: apps/*/envs/*   
+  template:    
+    metadata:
+      name: '{{index .path.segments 1}}-{{index .path.segments 3}}'   
+    spec:
+      # The project the application belongs to.
+      project: default
 
+      # Source of the application manifests
+      sources:
+      - repoURL: https://github.com/abinet/CICD-Demo-Deployment.git
+        targetRevision: add-config
+        path: 'apps/{{index .path.segments 1}}/helm'
+        helm:
+          releaseName: '{{index .path.segments 1}}'
+          valueFiles:
+          - '$values/apps/{{index .path.segments 1}}/envs/{{index .path.segments 3}}/values.yaml'
+      - repoURL: https://github.com/abinet/CICD-Demo-Deployment.git
+        targetRevision: add-config
+        ref: values
+      syncPolicy:
+        syncOptions:
+        - CreateNamespace=true
+      # Destination cluster and namespace to deploy the application
+      destination:
+        name: '{{index .path.segments 3}}-cluster'
+        namespace: '{{index .path.segments 1}}'
+EOT
   depends_on = [
     helm_release.argocd,
-    kubectl_manifest.example_app_ns
   ]
 }
